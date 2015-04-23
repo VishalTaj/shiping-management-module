433fe2d12838e2aca4ffbea9c2f3d9ea03a674a1
Vishal Taj PM <getvishalonline@gmail.com>
2015-04-23 14:52:51 +0530

    Create __openerp__.py

---
+++ __openerp__.py
@@ -0,0 +1,10 @@
+{
+"name" : "Ship",
+"version" : "1.1",
+"author" : "Vishal Taj PM",
+"category" : "Shipment Sales",
+"depends" : ["sale"],
+"data" : ["ship.xml","sale_order.xml"],
+"installable": True,
+"active": False,
+}
